9019d040ae89535d73c4767a9d481be0e5d27419
David Mebo <mebodave@gmail.com>
2023-04-05 23:15:54 +0100

    created manifest for creating a file in the /tmp/ folder

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Manifest creates a file in the /tmp directory. Best run in a VM
+
+file { '/tmp/school':
+  ensure  => 'present',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,3 @@
+# Configuration Management With Puppet
+
+Warning: Run the manifests only in a VM!
